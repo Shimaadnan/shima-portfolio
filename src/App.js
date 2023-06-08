2f578d5703261ceb97ea7dea30d142f9a7f1bd45
Shima <shimaadnanbasmenj@gmail.com>
2023-06-08 09:43:34 +0100

    Skills done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import About from "./components/About";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import Skills from "./components/Skills";
+import Works from "./components/Works";
 
 
 function App() {
@@ -9,6 +11,8 @@ function App() {
     <Navbar/>
     <Hero/>
     <About/>
+    <Skills/>
+    <Works/> 
     </div>
   );
 }
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -2,21 +2,42 @@ import React from 'react'
 import CSS from '../assets/css.png'
 import HTML from '../assets/html.png'
 import JavaScript from '../assets/javascript.png'
-import React from '../assets/react.png'
+import REACT from '../assets/react.png'
 import Node from '../assets/node.png'
 import Sql from '../assets/sql.png'
 export default function Skills() {
   return (
-    <div name='skills' className='bg-[#0a192f] text-gray-300'>
+    <div name='skills' className='w-full h-sreen bg-[#0a192f] text-gray-300'>
         
       <div className='max-w-[1000px] m-auto p-4 flex flex-col justify-center w-full h-full'>
         <div>
-            <p className='text-4xl font-bold inline border-b-4 border-pink-600'> Experiences</p>
-            <p > These are the Technologies I have worker with</p>
+            <p className='text-4xl font-bold inline border-b-4 border-pink-600'> Skills</p>
+            <p  className='py-4'>  These are the Technologies I have worker with</p>
         </div>
-        <div>
-            <div>
-                <img src={HTML} alt="html"/>
+        <div className='w-full grid grid-cols-2 sm:grid-cols-3 gap-4 text-center py-8'>
+            <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500' >
+                <img className='w-20 max-auto'src={HTML} alt="html"/>
+                <p className='my-4'>HTML</p>
+            </div>
+            <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500' >
+                <img className='w-20 max-auto'src={CSS} alt="css"/>
+                <p className='my-4'>CSS</p>
+            </div>
+            <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500' >
+                <img className='w-20 max-auto'src={JavaScript} alt="javascript"/>
+                <p className='my-4'>JavaScript</p>
+            </div>
+            <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500' >
+                <img className='w-20 max-auto'src={REACT} alt="react"/>
+                <p className='my-4'>React.js</p>
+            </div>
+            <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500' >
+                <img className='w-20 max-auto'src={Node} alt="node"/>
+                <p className='my-4'>Node.js</p>
+            </div>
+            <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500' >
+                <img className='w-20 max-auto'src={Sql} alt="sgl"/>
+                <p className='my-4'>SQL</p>
             </div>
         </div>
       </div>
