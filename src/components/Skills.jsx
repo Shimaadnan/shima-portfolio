ff8ba92d5dbdfb4295bb85213b007b8f53609743
Shima <shimaadnanbasmenj@gmail.com>
2023-06-09 09:45:52 +0100

    Works is done

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -12,7 +12,7 @@ export default function Skills() {
       <div className='max-w-[1000px] m-auto p-4 flex flex-col justify-center w-full h-full'>
         <div>
             <p className='text-4xl font-bold inline border-b-4 border-pink-600'> Skills</p>
-            <p  className='py-4'>  These are the Technologies I have worker with</p>
+            <p  className='py-6'>  These are the Technologies I have worker with</p>
         </div>
         <div className='w-full grid grid-cols-2 sm:grid-cols-3 gap-4 text-center py-8'>
             <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500' >
