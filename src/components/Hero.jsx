5b0b84c89c92168a584aaa243389865a0fab3601
Shima <shimaadnanbasmenj@gmail.com>
2023-06-12 13:43:51 +0100

    Contact page

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ export default function Hero() {
   return (
     <div name='hero' className='w-full h-screen bg-[#0a192f]'>
         {/* Container */}
-        <div className='mac-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
+        <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
           <p className='text-pink-600'>Hi, my name is</p>
           <h1 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>Shima Adnanbasmenj</h1>
           <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I am Full Stack Developer</h2>
