915ec57fb95e02e0a8a9f51160c8145e35aa3594
Shima <shimaadnanbasmenj@gmail.com>
2023-06-15 09:48:00 +0100

    Linked all the pages

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,7 +8,11 @@ export default function Hero() {
           <p className='text-pink-600'>Hi, my name is</p>
           <h1 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>Shima Adnanbasmenj</h1>
           <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I am Full Stack Developer</h2>
-        
+          <p className='text-[#8892b0] py-4 max-w-[700px]'>
+          Developer, holding a Master's degree in Mathematics with 10 years of teaching experience.
+           Recently graduated from Code Your Future, an intensive full-stack development program.
+             Participated in regular in-person and online software development classes and submitted weekly coursework, solving many problems for various software development technologies
+        </p>
         <div>
     <button className='text-white group  border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600  hover:border-pink-600'> 
     View Works
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import React,{useState} from 'react'
 import {FaBookOpen,FaBars,FaTimes,FaLinkedin,FaGithub} from "react-icons/fa";
 import { HiOutlineMail } from 'react-icons/hi';
 import {BsFillPersonLinesFill} from 'react-icons/bs'
+import {Link} from 'react-scroll'
 
 export default function Navbar() {
   const[nav,setNav]=useState(false)
@@ -14,11 +15,22 @@ export default function Navbar() {
     {/* Menu */}
     
       <ul className='hidden md:flex'>
-        <li>Home</li>
-        <li>About</li>
-        <li>Skills</li>
-        <li>Works</li>
-        <li>Contact</li>
+        <li>
+        <Link  to="hero"  smooth={true}  duration={500} >
+          Home
+        </Link></li>
+        <li><Link  to="about"  smooth={true}  duration={500} >
+          About
+        </Link></li>
+        <li><Link  to="skills"  smooth={true}  duration={500} >
+          Skills
+        </Link></li>
+        <li><Link  to="work"  smooth={true}  duration={500} >
+          Works
+        </Link></li>
+        <li><Link  to="contact"  smooth={true}  duration={500} >
+          Contact
+        </Link></li>
       </ul>
     
     {/* Hamburger */}
@@ -29,11 +41,21 @@ export default function Navbar() {
     {/* Mobile menu */}
     
     <ul className={!nav ? 'hidden':'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'}>
-        <li className='py-6 text-4xl'>Home</li> 
-        <li className='py-6 text-4xl'>About</li>
-        <li className='py-6 text-4xl'>Skills</li>
-        <li className='py-6 text-4xl'>Works</li>
-        <li className='py-6 text-4xl'>Contact</li>
+        <li className='py-6 text-4xl'><Link onClick={handleclick } to="hero"  smooth={true}  duration={500} >
+          Home
+        </Link></li> 
+        <li className='py-6 text-4xl'><Link onClick={handleclick } to="about"  smooth={true}  duration={500} >
+          About
+        </Link></li>
+        <li className='py-6 text-4xl'><Link onClick={handleclick } to="skills"  smooth={true}  duration={500} >
+          Skills
+        </Link></li>
+        <li className='py-6 text-4xl'><Link onClick={handleclick } to="work"  smooth={true}  duration={500} >
+          Work
+        </Link></li>
+        <li className='py-6 text-4xl'><Link onClick={handleclick } to="contact"  smooth={true}  duration={500} >
+          Contact
+        </Link></li>
       </ul>
     
     {/* Social Media */}
